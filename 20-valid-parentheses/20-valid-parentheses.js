897f36c984f12975bb575c0287a8750cb9e62945
Abdullah Tabbal <59699889+AbdullahPS@users.noreply.github.com>
2022-06-28 20:10:43 +0300

    Time: 111 ms (20.18%), Space: 44 MB (11.06%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+
+ const validString=new Map(
+      [[')','('],['}','{'],[']','[']])  
+ const stack=[];
+for(let currS of s){
+    stack.push(currS);
+    if(currS==='}'||currS===']'||currS===')'){
+        const index=stack.indexOf(currS)
+        if(stack[index-1]!==validString.get(currS))
+            return false
+        stack.splice(index-1,index+1)
+    }
+}
+    console.log(stack)
+     return stack.length===0
+
+ 
+ };
+
+
+
+
